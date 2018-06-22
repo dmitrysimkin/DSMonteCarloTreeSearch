c519b15e31ab12db6f9aab994ebd4b64272e7062
Simkin Dmitry <dsimkin@luware.com>
2018-06-22 09:28:55 +0300

    Added comments to DSState class

--- DSMonteCarloTreeSearch/DSState.swift
+++ DSMonteCarloTreeSearch/DSState.swift
@@ -8,16 +8,55 @@
 
 import Foundation
 
+/// Interface describing state that can be passed to Monte Carlo Tree Search algorithm
 public protocol DSStateProtocol: Equatable {
+
+    /// Possible Transitions
+    ///
+    /// - returns: list of possible transitions from current state
     func possibleTransitions() -> [DSTransition]
+    
+    
+    /// Return state that happens after applying 'transition' to current state
+    ///
+    /// - parameters:
+    ///   - afterTransition: transition to apply
+    /// - return: state that happens after applying 'transition' to current state
     func state(afterTransition transition:DSTransition) -> DSState
+    
+    
+    /// Simulate from current state to a terminal state and return value comparing to 'againstState'
+    /// that represents win, loss, draw, etc. (you have to choose appropriate value)
+    ///
+    /// - parameters:
+    ///   - againstState: root ndoe's state to evaluate simulation
+    /// - returns: simulated value
     func simulate(againstState state: DSState) -> Int
+    
+    
+    /// Return whether state is terminal or not
     var isTerminal: Bool { get }
+    
+    /// Transition that led to current state
     var transition: DSTransition { get }
+    
+    /// Return wheteer current state is equal to 'rhs' state
+    ///
+    /// - parameters:
+    ///   - rhs: object to compare to
+    /// -retunrs: equal or not
     func equalTo(rhs: DSState) -> Bool
 }
 
+
+/// Abstact class to inherit from and adapt to your domain
+/// Make a subclass and implement 'DSStateProtocol' methods
 open class DSState: DSStateProtocol {
+
+    /// Inits state with transition that led to that state
+    ///
+    /// - parameters:
+    ///   - transition: transition that led to that state
     public init(transition: DSTransition) {
         self.transition = transition
     }
